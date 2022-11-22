8c4d0fd2642a595c83d3fc2cfd29e831e0b9b7e0
burak.akkaya <burak.akkaya@pingpong.university>
2022-11-22 11:35:54 +0300

    case one

---
+++ 1/Program.cs
@@ -0,0 +1,12 @@
+﻿string uniqueString = StringUtil.GetUniqueString(8);
+Console.WriteLine("Unique String : " + uniqueString);
+
+string encrypted = StringUtil.Encrypt(uniqueString);
+Console.WriteLine("Encrypted : " + encrypted);
+
+string decrypted = StringUtil.Decrypt(encrypted);
+Console.WriteLine("Decrypted : " + decrypted);
+
+
+Console.ReadLine();
+
